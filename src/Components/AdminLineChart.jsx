59af7bc614900bebe1def1c8fe19b21a7886f012
Asindu De Silva <asindudesilva@gmail.com>
2023-08-25 20:46:47 +0530

    Created Weekly Sales chart

---
+++ src/Components/AdminLineChart.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
+import MyBackdrop from './MyBackdrop';
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+  );
+  
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' ,
+      },
+    },
+  };
+
+  const currentTime = new Date();
+  const currentOffset = currentTime.getTimezoneOffset();
+  const ISTOffset = 330;  
+  const ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);  
+  
+  const labels = [];
+  
+  for(let i = 7; i > 0; i--){
+    labels.push(ISTTime.getMonth() + "/" + (ISTTime.getDate() - i));
+  }
+
+export default function AdminLineChart() {
+
+  const {backendAddress} = useSelector(state => state.backendInfo);
+  const [orderCountList, setOrderCountList] = useState([]);  
+  const [backDropOpen, setBackDropOpen] = useState(false);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: 'Orders',
+        data: orderCountList,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  
+  const loadOrderCountList = () => {
+    setBackDropOpen(true);
+    fetch(`http://${backendAddress}/order/week`, {
+        headers: {
+            'Authorization': `Bearer ${Cookies.get('jwt')}`,
+        },
+    })
+      .then(res => res.json())
+      .then(data => {
+        setBackDropOpen(false);
+        setOrderCountList(data);
+      })
+  }
+
+  useEffect(() => {
+    loadOrderCountList();
+  }, []);
+
+  return (
+    <>
+      <Line options={options} data={data} />
+      <MyBackdrop backDropOpen={backDropOpen} />
+    </>
+  )
+}
