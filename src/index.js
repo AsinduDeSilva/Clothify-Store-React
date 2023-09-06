788fd0a26f09a8574876fc16e25266a5cffffb25
Asindu De Silva <asindudesilva@gmail.com>
2023-09-06 12:24:48 +0530

    Implemented necessary changes for deploy on Netlify

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Clothify-Store-React'>
+    <BrowserRouter>
       <Provider store={Store}>
         <ThemeProvider>
          <App />
